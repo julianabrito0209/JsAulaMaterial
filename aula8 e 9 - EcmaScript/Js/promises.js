b2983a26244575aa28fb756babd32ac4271fcfc4
julianabrito0209 <juliana0209@gmail.com>
2023-08-22 16:35:49 -0300

    Promise

---
+++ aula8 e 9 - EcmaScript/Js/promises.js
@@ -0,0 +1,9 @@
+//CRIANDO UMA PROMISE
+const primeiraPromise = new Promise((resolve, reject) => {
+    const nomeP = "Victor"; 
+}
+if(nomeP ==="Victor"){
+    resolve("O usuário é Victor!")
+}else{
+    reject("O usuário não é permitido!")
+});

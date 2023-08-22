92a867e715234d47987569b3b2a84bb6bfb29f7a
julianabrito0209 <juliana0209@gmail.com>
2023-08-22 17:56:04 -0300

    Solução da atividade 1
    Array e

--- aula8 e 9 - EcmaScript/Exercícios/Ativ1.js
+++ aula8 e 9 - EcmaScript/Exercícios/Ativ1.js
@@ -1,1 +1,5 @@
 //1) Fazer um array de filmes e listar cada elemento com a formatação = "Filme 1 : {elemento}"
+
+var filmes = ["Black Panther", "Capitã Marvel", "O incrível Hulk", "Thor Ragnarok", "Homem de Ferro"]
+var listaFilmes = filmes.map((filme) => "<li> Filme :" +filme+ "</li>")
+document.getElementById("mapFilmes").innerHTML = `<ul>${listaFilmes}</ul>`

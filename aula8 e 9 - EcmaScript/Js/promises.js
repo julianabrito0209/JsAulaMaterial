2e66fa49798fc320d9f60fa601c89cd18a719ca8
julianabrito0209 <juliana0209@gmail.com>
2023-08-22 17:17:34 -0300

    Async & Await

--- aula8 e 9 - EcmaScript/Js/promises.js
+++ aula8 e 9 - EcmaScript/Js/promises.js
@@ -1,9 +1,40 @@
 //CRIANDO UMA PROMISE
+
 const primeiraPromise = new Promise((resolve, reject) => {
     const nomeP = "Victor"; 
+
+
+    if(nomeP === "Victor"){
+        resolve("O usuário é Victor!")
+    }else{
+        reject("O usuário não é permitido!")
+    }
+})
+
+//primeiraPromise.catch - para apontar erro 
+
+primeiraPromise.then((p) =>{
+    console.log(p);
+})
+
+//await
+function sujeitoDemorado(){
+    return new Promise ((resolve)=> {
+        setTimeout(() => {
+            console.log("Carregou o sujeito")
+            resolve()
+        },2000)
+    });
 }
-if(nomeP ==="Victor"){
-    resolve("O usuário é Victor!")
-}else{
-    reject("O usuário não é permitido!")
-});
+//dentro de uma função com uso de async + await = async vai declarar que aquela função não precisa obedecer a ordem e tem prioridade.  E o await tem que ser colocado à frente do termo que precisa 
+async function carregando(){
+    console.log("Iniciei")
+    await sujeitoDemorado()
+    console.log("fechei")
+
+}
+carregando()
+
+
+//perguntar sobre o e-mail do senai. 
+//até quando vai funcionar?

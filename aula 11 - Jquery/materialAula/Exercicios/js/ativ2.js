3736089d28f5fc687b3cab799d9a42bb091edda1
JULIANA NUNES DE BRITO SILVA <juliana0209@gmail.com>
2023-08-25 10:20:47 -0300

    v1- ativ 2 - aula 11

--- aula 11 - Jquery/materialAula/Exercicios/js/ativ2.js
+++ aula 11 - Jquery/materialAula/Exercicios/js/ativ2.js
@@ -3,23 +3,6 @@
 
 
 
-
-//jQuery ("." ou "#") - seletor do jQuery
-//jQuery = $ (versão resumida)
-
-var paragrafoLorem = jQuery("#paragrafoLorem").text()
-
-// text() - lista o valor
-// text("ELEMENTO") - sobrescreve o valor 
-const usuario = "juliana"; 
-var usuarioHtml = $("#username").text(usuario);
-
-console.log(paragrafoLorem)
-
-var quantidadeDePalavras = paragrafoLorem.split(" ").length;
-
-// split pega o texto e "picota" ele, colocando entre os parenteses o que deve ser contado " " ou "."
-
 //quantidade dinamica
 
 var areaTexto = $("#areaTexto")
